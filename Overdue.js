3db8b73aa32b1eb8ab6063a98c031d1aa3f8182b
lynnsimonen <79551770+lynnsimonen@users.noreply.github.com>
2021-03-29 21:55:07 -0500

    Initial commit

---
+++ Overdue.js
@@ -0,0 +1,26 @@
+function calculate()
+{
+    //gather all input
+    //ask user for Total number of days late
+    //convert input to number type
+    let numDays = parseFloat( prompt("How many days late are your library items?"));
+
+
+    //ask for number of books
+    //convert to number type
+    let numBooks = parseFloat((prompt("How many books?")));
+
+    //ask for number of dvds
+    //convert to number type
+    let numDVDs = parseFloat((prompt("How many DVDs?")));
+
+    //do some math...
+    //((numBooks * daily fee) + (numDVDs * daily fee)) * numDays
+    let totalFee = ((numBooks * .25) + (numDVDs * .5)) * numDays;
+
+
+    //OUTPUT
+    //alert the gas cost
+    alert(`You owe the Waukesha Public Library $${totalFee.toFixed(2)} in late fees.`);
+
+}
